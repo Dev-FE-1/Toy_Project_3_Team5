f2903a40d4302c9dcf4799dd24aa4d6b252510f4
meryoung <dev.meryoung@gmail.com>
2024-08-24 01:58:02 +0900

    settings: 프로젝트 초기 세팅 수정사항 반영

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,9 @@ module.exports = {
 		'airbnb/hooks',
 		'plugin:@typescript-eslint/recommended',
 		'plugin:prettier/recommended',
+		'plugin:import/errors',
+		'plugin:import/warnings',
+		'plugin:import/typescript',
 	],
 	parserOptions: {
 		ecmaVersion: 2020,
@@ -13,15 +16,39 @@ module.exports = {
 			jsx: true,
 		},
 	},
+	plugins: ['import-order'],
 	rules: {
 		'@typescript-eslint/no-unused-vars': ['error'],
 		'prettier/prettier': 'error',
+		'react/react-in-jsx-scope': 'off',
+		'react/jsx-filename-extension': [
+			1,
+			{ extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+		],
 		'import/no-extraneous-dependencies': [
 			'error',
 			{
 				devDependencies: true,
 				optionalDependencies: false,
 				peerDependencies: false,
+			},
+		],
+		'import/order': [
+			'error',
+			{
+				groups: ['builtin', 'external', 'internal', 'index'],
+				pathGroups: [
+					{
+						pattern: 'react',
+						group: 'external',
+						position: 'before',
+					},
+				],
+				pathGroupsExcludedImportTypes: ['react'],
+				alphabetize: {
+					order: 'asc',
+					caseInsensitive: true,
+				},
 			},
 		],
 	},

8ae6348dd8d62b547f2d1964c40600d3502c16cb
hjkwon <sirru97@naver.com>
2024-08-26 01:08:21 +0900

    fix: prettier 에러 해결 #13

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,78 +1,78 @@
 module.exports = {
-	parser: '@typescript-eslint/parser',
-	extends: [
-		'airbnb',
-		'airbnb/hooks',
-		'plugin:@typescript-eslint/recommended',
-		'plugin:prettier/recommended',
-		'plugin:import/errors',
-		'plugin:import/warnings',
-		'plugin:import/typescript',
-	],
-	parserOptions: {
-		ecmaVersion: 2020,
-		sourceType: 'module',
-		ecmaFeatures: {
-			jsx: true,
-		},
-	},
-	plugins: [
-		'@typescript-eslint',
-		'react',
-		'prettier',
-		'import',
-		'import-order',
-	],
-	rules: {
-		'@typescript-eslint/no-unused-vars': ['error'],
-		'prettier/prettier': ['error'],
-		'react/react-in-jsx-scope': 'off',
-		'react/jsx-filename-extension': [
-			1,
-			{ extensions: ['.js', '.jsx', '.ts', '.tsx'] },
-		],
-		'react/function-component-definition': [
-			2,
-			{
-				namedComponents: 'arrow-function',
-				unnamedComponents: 'arrow-function',
-			},
-		],
-		'import/no-extraneous-dependencies': [
-			'error',
-			{
-				devDependencies: true,
-				optionalDependencies: false,
-				peerDependencies: false,
-			},
-		],
-		'import/order': [
-			'error',
-			{
-				groups: ['builtin', 'external', 'internal', 'index'],
-				pathGroups: [
-					{
-						pattern: 'react',
-						group: 'external',
-						position: 'before',
-					},
-				],
-				pathGroupsExcludedImportTypes: ['react'],
-				alphabetize: {
-					order: 'asc',
-					caseInsensitive: true,
-				},
-			},
-		],
-	},
-	settings: {
-		react: {
-			version: 'detect',
-		},
-		'import/resolver': {
-			typescript: {
-				project: './tsconfig.json',
-			},
-		},
-	},
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+  ],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: [
+    '@typescript-eslint',
+    'react',
+    'prettier',
+    'import',
+    'import-order',
+  ],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error'],
+    'prettier/prettier': ['error'],
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-filename-extension': [
+      1,
+      { extensions: ['.js', '.jsx', '.ts', '.tsx'] },
+    ],
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
+    'import/no-extraneous-dependencies': [
+      'error',
+      {
+        devDependencies: true,
+        optionalDependencies: false,
+        peerDependencies: false,
+      },
+    ],
+    'import/order': [
+      'error',
+      {
+        groups: ['builtin', 'external', 'internal', 'index'],
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        alphabetize: {
+          order: 'asc',
+          caseInsensitive: true,
+        },
+      },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      typescript: {
+        project: './tsconfig.json',
+      },
+    },
+  },
 };
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import GlobalStyles from '@/styles/GlobalStyles.tsx';
 
 const App = () => {
-	return (
-		<div>
-			<GlobalStyles />
-			<p>App</p>
-		</div>
-	);
+  return (
+    <div>
+      <GlobalStyles />
+      <p>App</p>
+    </div>
+  );
 };
 
 export default App;

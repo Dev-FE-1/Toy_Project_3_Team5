812af53d4ae66ea8aed385e8a650ee3c23485d94
kimisadev27 <kimisadev27@gmail.com>
2024-08-26 19:00:13 +0900

    feature: 개별영상(소) 컴포넌트 개발 (#5)

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,52 +1,54 @@
 import { css } from '@emotion/react';
+import colors from '@/constants/colors';
+import { fontSize, fontWeight } from '@/constants/font';
 
 interface VideoProps {
-	// link: string;
-	imgUrl: string;
-	title: string;
-	userName: string;
+  imgUrl: string;
+  title: string;
+  userName: string;
 }
 
-const Video = ({ imgUrl, title, userName }: VideoProps) => {
-	return (
-		<div css={VideoContainer}>
-			<img css={ThumbnailStyle} src={imgUrl} alt='썸네일' />
-			<div css={VideoInfoStyle}>
-				<span css={TitleStyle}>{title}</span>
-				<span css={UserNameStyle}>{userName}</span>
-			</div>
-		</div>
-	);
-};
+const Video = ({ imgUrl, title, userName }: VideoProps) => (
+  <div css={VideoContainer}>
+    <img css={ThumbnailStyle} src={imgUrl} alt='썸네일' />
+    <div css={VideoInfoStyle}>
+      <span css={TitleStyle}>{title}</span>
+      <span css={UserNameStyle}>{userName}</span>
+    </div>
+  </div>
+);
 
 const VideoContainer = css`
-	display: flex;
-	cursor: pointer;
-	margin-bottom: 10px;
+  display: flex;
+  cursor: pointer;
+  margin-bottom: 10px;
+  padding: 0px 10px;
 `;
 
 const ThumbnailStyle = css`
-	width: 200px;
-	height: 120px;
-	margin-right: 10px;
-	border-radius: 10px;
-	/* padding: 10px; */
+  width: 200px;
+  height: 120px;
+  margin-right: 10px;
+  border-radius: 10px;
 `;
 
 const VideoInfoStyle = css`
-	display: flex;
-	flex-direction: column;
-	padding: 5px 10px;
+  display: flex;
+  flex-direction: column;
+  padding: 5px 10px;
 `;
 const TitleStyle = css`
-	color: gray;
+  font-size: ${fontSize.xl};
+  font-weight: ${fontWeight.medium};
+  color: ${colors.black};
 `;
 const UserNameStyle = css`
-	color: black;
+  font-size: ${fontSize.md};
+  color: ${colors.gray05};
 
-	&:hover {
-		text-decoration: underline;
-	}
+  &:hover {
+    text-decoration: underline;
+  }
 `;
 
 export default Video;

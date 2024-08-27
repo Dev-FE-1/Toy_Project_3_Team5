1978876c590f5adfd60aad4f07db383242b5e621
meryoung <dev.meryoung@gmail.com>
2024-08-27 17:14:10 +0900

    feature: 누락된 props 추가, 아이콘 사용 방식 변경 및 CSS Size, Color 관련 로직 수정 #7

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { css } from '@emotion/react';
-import { Plus, LogOut } from 'lucide-react';
-import googleIcon from '@/assets/google_icon.svg';
 import colors from '@/constants/colors';
 
 type ButtonShapeType = 'block' | 'line' | 'round' | 'text';
 type ButtonSizeType = 'sm' | 'md' | 'lg';
 type ButtonColorType = 'primary' | 'gray' | 'red' | 'black';
-type ButtonIconType = 'plus' | 'logout' | 'google' | undefined;
+type ButtonIconType = React.ComponentType<IconProps> | string | undefined;
+
+interface IconProps {
+  size?: string | number;
+}
 
 interface ButtonProps {
   label: string;
@@ -15,9 +17,34 @@ interface ButtonProps {
   shape?: ButtonShapeType;
   size?: ButtonSizeType;
   color?: ButtonColorType;
-  icon?: ButtonIconType;
+  IconComponent?: ButtonIconType;
   fullWidth?: boolean;
+  disabled?: boolean;
 }
+
+type ButtonColors = {
+  color: string;
+  hoverColor: string;
+};
+
+const buttonColors: Record<ButtonColorType, ButtonColors> = {
+  primary: { color: colors.primaryNormal, hoverColor: colors.primaryDark },
+  gray: { color: colors.gray05, hoverColor: colors.gray06 },
+  red: { color: colors.redNormal, hoverColor: colors.redDark },
+  black: { color: colors.black, hoverColor: colors.gray04 },
+};
+
+const buttonSizes: Record<ButtonSizeType, ReturnType<typeof css>> = {
+  sm: css`
+    padding: 4px 12px;
+  `,
+  md: css`
+    padding: 8px 12px;
+  `,
+  lg: css`
+    padding: 14px 12px;
+  `,
+};
 
 const Button: React.FC<ButtonProps> = ({
   label,
@@ -25,25 +52,32 @@ const Button: React.FC<ButtonProps> = ({
   shape = 'block',
   size = 'md',
   color = 'primary',
-  icon = undefined,
+  IconComponent = undefined,
   fullWidth = false,
+  disabled = false,
 }) => {
-  const renderIcon = () => {
-    switch (icon) {
-      case 'plus':
-        return <Plus size={16} />;
-      case 'logout':
-        return <LogOut size={16} />;
-      case 'google':
-        return <img src={googleIcon} width={16} height={16} />;
-      default:
-        return null;
-    }
-  };
+  const selectColors = buttonColors[color];
+  const selectSizes = buttonSizes[size];
 
   return (
-    <button css={buttonStyle(shape, size, color, fullWidth)} onClick={onClick}>
-      {icon && <span style={{ marginRight: '6px' }}>{renderIcon()}</span>}
+    <button
+      css={buttonStyle(shape, selectSizes, selectColors, fullWidth, disabled)}
+      onClick={onClick}
+      disabled={disabled}
+    >
+      {IconComponent && (
+        <span style={{ marginRight: '6px' }}>
+          {typeof IconComponent === 'string' ? (
+            <img
+              src={IconComponent}
+              alt='아이콘'
+              style={{ width: 16, height: 16 }}
+            />
+          ) : (
+            <IconComponent size={16} />
+          )}
+        </span>
+      )}
       {label}
     </button>
   );
@@ -51,112 +85,47 @@ const Button: React.FC<ButtonProps> = ({
 
 const buttonStyle = (
   shape: ButtonShapeType,
-  size: ButtonSizeType,
-  color: ButtonColorType,
-  fullWidth: boolean
+  selectSizes: ReturnType<typeof css>,
+  selectColors: ButtonColors,
+  fullWidth: boolean,
+  disabled: boolean
 ) => css`
   display: inline-flex;
   align-items: center;
   justify-content: center;
-  cursor: pointer;
+  cursor: ${disabled ? 'not-allowed' : 'pointer'};
   border: none;
   outline: none;
   width: ${fullWidth ? '100%' : 'auto'};
+  border-radius: ${shape === 'round' ? '50px' : '6px'};
+
+  ${selectSizes}
+
+  background-color: ${disabled
+    ? colors.gray03
+    : shape === 'text' || shape === 'line'
+      ? 'transparent'
+      : selectColors.color};
+  color: ${disabled
+    ? colors.black
+    : shape === 'text' || shape === 'line'
+      ? selectColors.color
+      : colors.white};
+  border: ${shape === 'line' ? `1px solid ${selectColors.color}` : 'none'};
+  opacity: ${disabled ? 0.7 : 1};
+
+  &:hover {
+    ${!disabled &&
+    `background-color: ${
+      shape === 'text' ? 'transparent' : selectColors.hoverColor
+    };
+    color: ${shape === 'text' ? selectColors.hoverColor : colors.white};
+    ${shape === 'line' && `border-color: ${selectColors.hoverColor}`};`}
+  }
 
   span {
     display: inline-flex;
   }
-
-  ${(shape === 'block' || shape === 'line') &&
-  css`
-    border-radius: 6px;
-  `}
-
-  ${shape === 'round' &&
-  css`
-    border-radius: 50px;
-  `}
-
-    ${size === 'sm' &&
-  css`
-    padding: 4px 12px;
-  `}
-
-    ${size === 'md' &&
-  css`
-    padding: 8px 12px;
-  `}
-
-  ${size === 'lg' &&
-  css`
-    padding: 14px 12px;
-  `}
-
-  ${color === 'primary' &&
-  css`
-    background-color: ${shape === 'text' || shape === 'line'
-      ? 'transparent'
-      : colors.primaryNormal};
-    color: ${shape === 'text' || shape === 'line'
-      ? colors.primaryNormal
-      : colors.white};
-    border: ${shape === 'line' ? `1px solid ${colors.primaryNormal}` : 'none'};
-    &:hover {
-      background-color: ${shape === 'text'
-        ? 'transparent'
-        : colors.primaryDark};
-      color: ${shape === 'text' ? colors.primaryDark : colors.white};
-      ${shape === 'line' && `border-color: ${colors.primaryDark}`};
-    }
-  `}
-
-  ${color === 'gray' &&
-  css`
-    background-color: ${shape === 'text' || shape === 'line'
-      ? 'transparent'
-      : colors.gray05};
-    color: ${shape === 'text' || shape === 'line'
-      ? colors.gray05
-      : colors.white};
-    border: ${shape === 'line' ? `1px solid ${colors.gray05}` : 'none'};
-    &:hover {
-      background-color: ${shape === 'text' ? 'transparent' : colors.gray06};
-      color: ${shape === 'text' ? colors.gray06 : colors.white};
-      ${shape === 'line' && `border-color: ${colors.gray06}`};
-    }
-  `}
-
-  ${color === 'red' &&
-  css`
-    background-color: ${shape === 'text' || shape === 'line'
-      ? 'transparent'
-      : colors.redNormal};
-    color: ${shape === 'text' || shape === 'line'
-      ? colors.redNormal
-      : colors.white};
-    border: ${shape === 'line' ? `1px solid ${colors.redNormal}` : 'none'};
-    &:hover {
-      background-color: ${shape === 'text' ? 'transparent' : colors.redDark};
-      color: ${shape === 'text' ? colors.redDark : colors.white};
-      ${shape === 'line' && `border-color: ${colors.redDark}`};
-    }
-  `}
-
-  ${color === 'black' &&
-  css`
-    background-color: ${shape === 'text' || shape === 'line'
-      ? 'transparent'
-      : colors.black};
-    color: ${shape === 'text' || shape === 'line'
-      ? colors.black
-      : colors.white};
-    border: ${shape === 'line' ? `1px solid ${colors.black}` : 'none'};
-    &:hover {
-      background-color: ${shape === 'text' ? 'transparent' : colors.gray04};
-      color: ${shape === 'text' ? colors.gray04 : colors.white};
-      ${shape === 'line' && `border-color: ${colors.gray04}`};
-    }
-  `}
 `;
 
 export default Button;

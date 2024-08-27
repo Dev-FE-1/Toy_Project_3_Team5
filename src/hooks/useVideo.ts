219b32124afa438efb0c780a038d83837ca46f3e
Steven <ldh921126@gmail.com>
2024-08-27 15:28:03 +0900

    feature: firebase 환경 설정 #22

--- src/hooks/useVideo.ts
+++ src/hooks/useVideo.ts
@@ -1,31 +1,31 @@
 import { useEffect, useState } from 'react';
 
 const TestData = {
-	imgUrl: '/src/assets/defaultThumbnail.jpg',
-	title: 'lofi hip hop radio 📚 - beats to relax/study to2',
-	userName: 'Lofi Girl2',
+  imgUrl: '/src/assets/defaultThumbnail.jpg',
+  title: 'lofi hip hop radio 📚 - beats to relax/study to2',
+  userName: 'Lofi Girl2',
 };
 
 const useVideo = () => {
-	const [imgUrl, setImgUrl] = useState<string>('');
-	const [title, setTitle] = useState<string>('');
-	const [userName, setUserName] = useState<string>('');
+  const [imgUrl, setImgUrl] = useState<string>('');
+  const [title, setTitle] = useState<string>('');
+  const [userName, setUserName] = useState<string>('');
 
-	// convert link to video info
-	// const result = await convertLinkToVideoInfo(link);
+  // convert link to video info
+  // const result = await convertLinkToVideoInfo(link);
 
-	// set video info
-	// setImgUrl(result.imgUrl);
-	// setTitle(result.title);
-	// setUserName(result.userName);
-	useEffect(() => {
-		setImgUrl(TestData.imgUrl);
-		setTitle(TestData.title);
-		setUserName(TestData.userName);
-	}, []);
+  // set video info
+  // setImgUrl(result.imgUrl);
+  // setTitle(result.title);
+  // setUserName(result.userName);
+  useEffect(() => {
+    setImgUrl(TestData.imgUrl);
+    setTitle(TestData.title);
+    setUserName(TestData.userName);
+  }, []);
 
-	// return video info
-	return { imgUrl, title, userName };
+  // return video info
+  return { imgUrl, title, userName };
 };
 
 export default useVideo;

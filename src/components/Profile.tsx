69d83fb5a7e9d31f86068b78cc21de4ca250172d
Sumin <tnalsdldkr2@naver.com>
2024-08-28 03:19:25 +0900

    feature: Profile 컴포넌트 구현 #12

---
+++ src/components/Profile.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { css } from '@emotion/react';
+
+type ProfileProps = {
+  src: string;
+  alt: string;
+  size?: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
+};
+
+const sizes = {
+  xs: 32,
+  sm: 36,
+  md: 48,
+  lg: 100,
+  xl: 120,
+};
+
+const Profile: React.FC<ProfileProps> = ({ src, alt, size = 'lg' }) => (
+  <div
+    css={css`
+      width: ${sizes[size]}px;
+      height: ${sizes[size]}px;
+      border-radius: 50%;
+      overflow: hidden;
+      display: inline-block;
+    `}
+  >
+    <img
+      src={src}
+      alt={alt}
+      css={css`
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+      `}
+    />
+  </div>
+);
+
+export default Profile;

449c2b05940a5d457b7356867dbb44341b70ec1d
meryoung <dev.meryoung@gmail.com>
2024-08-27 20:23:33 +0900

    feature: Logo 컴포넌트 UI 구현 #10

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -3,17 +3,26 @@ import { css } from '@emotion/react';
 import logo from '@/assets/logo.svg';
 
 interface LogoProps {
+  logoWidth: number;
   clickable?: boolean;
 }
 
-const Logo: React.FC<LogoProps> = () => {
+const Logo: React.FC<LogoProps> = ({ logoWidth, clickable = false }) => {
   const onLogoClick = () => {};
 
   return (
-    <img css={LogoStyle} src={logo} alt='위플리 로고' onClick={onLogoClick} />
+    <img
+      css={LogoStyle(logoWidth, clickable)}
+      src={logo}
+      alt='위플리 로고'
+      onClick={clickable ? onLogoClick : undefined}
+    />
   );
 };
 
-const LogoStyle = css``;
+const LogoStyle = (logoWidth: number, clickable: boolean) => css`
+  width: ${logoWidth}px;
+  ${clickable && `cursor: pointer;`}
+`;
 
 export default Logo;

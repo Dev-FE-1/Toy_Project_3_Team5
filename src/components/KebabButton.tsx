a35052da57330eee98ba62ccef34ed847b4fef73
hjkwon <sirru97@naver.com>
2024-08-29 19:51:27 +0900

    fix: 공통 컴포넌트 수정 #74

--- src/components/KebabButton.tsx
+++ src/components/KebabButton.tsx
@@ -64,13 +64,14 @@ const kebabButtonStyle = css`
 
 const menuModalStyle = css`
   position: absolute;
-  top: 28px;
-  right: 0;
+  top: 30px;
+  right: 8px;
   width: 124px;
   background-color: ${colors.white};
   border-radius: 4px;
+  z-index: 10;
   box-shadow:
-    0px 3px 14px 2px rgba(0, 0, 0, 0.12),
+    0px 3px 8px 2px rgba(0, 0, 0, 0.12),
     0px 8px 10px 1px rgba(0, 0, 0, 0.14),
     0px 5px 5px -3px rgba(0, 0, 0, 0.2);
 

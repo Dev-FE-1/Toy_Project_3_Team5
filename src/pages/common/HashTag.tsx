0b3146f0afb6340f93d1f2839572166e07b15985
Sumin <tnalsdldkr2@naver.com>
2024-09-03 11:35:28 +0900

    feature: 해시태그 클릭시 NEXT 구현 #61

--- src/pages/common/HashTag.tsx
+++ src/pages/common/HashTag.tsx
@@ -8,23 +8,21 @@ import colors from '@/constants/colors';
 import { fontSize, fontWeight } from '@/constants/font';
 import { HASHTAGS } from '@/constants/hashtag';
 import ROUTES from '@/constants/route';
+import useTagSelection from '@/hooks/useTagSelection';
 
 const HashTag: React.FC = () => {
   const [tags, setTags] = useState<Tag[]>(HASHTAGS);
-  const [selectedTags, setSelectedTags] = useState<string[]>([]);
+  const { selectedTags, onTagSelection } = useTagSelection();
   const navigate = useNavigate();
 
   const onTagRemove = (id: number) => {
     setTags(tags.filter((tag) => tag.id !== id));
+    onTagSelection(id, true);
   };
 
-  const onSelected = (tag: string) => {
-    if (!selectedTags.includes(tag)) {
-      setSelectedTags([...selectedTags, tag]);
-    } else {
-      setSelectedTags(
-        selectedTags.filter((selectedTag) => selectedTag !== tag)
-      );
+  const onTagClick = (id: number, removable: boolean) => {
+    if (!removable) {
+      onTagSelection(id, removable);
     }
   };
 
@@ -37,14 +35,19 @@ const HashTag: React.FC = () => {
       <div css={titleStyle}>해시태그</div>
       <span css={desStyle}>관심 있는 태그를 선택해주세요 (최대 10개)</span>
       <div css={hashtagStyle}>
-        <HashTagComponent tags={tags} onRemove={onTagRemove} margin='8px' />
+        <HashTagComponent
+          tags={tags}
+          onRemove={onTagRemove}
+          onTagClick={onTagClick}
+          margin='8px'
+        />
       </div>
       <div css={skipContainerStyle}>
         <div onClick={onSkip} css={skipStyle}>
           {selectedTags.length > 0 ? 'NEXT' : 'SKIP'}
           <IconButton
             IconComponent={ChevronRight}
-            onClick={() => {}}
+            onClick={onSkip}
             size='md'
             color='black'
           />

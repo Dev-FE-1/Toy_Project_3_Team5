d49c5186e87c2ce54d432fc290bd2e21aac7cf30
kimisadev27 <kimisadev27@gmail.com>
2024-09-04 12:31:13 +0900

    feature: 플레이리스트 관리 코드 커스텀훅으로 이동 (#116)

---
+++ src/constants/playlist.ts
@@ -0,0 +1,37 @@
+export const TEXT = {
+  toggle: { active: '공개', inactive: '비공개' },
+  title: {
+    label: '플레이리스트 제목',
+    placeholder: '플레이리스트 제목을 입력해주세요.',
+    required: '제목을 입력해주세요.',
+  },
+  desc: {
+    label: '플레이리스트 설명',
+    placeholder: '플레이리스트에 대한 설명을 남겨주세요.',
+  },
+  link: {
+    label: '영상 링크 추가',
+    placeholder: '영상 링크를 입력해주세요.',
+    validmsg: '지원하지 않는 영상입니다.',
+    validDuplmsg: '중복된 링크입니다.',
+    required: '영상링크를 1개이상 입력해주세요.',
+  },
+  hashtag: {
+    label: '해시태그',
+    desc: '최대 5개까지 입력 가능합니다.',
+    placeholder: '#',
+    required: '해시태그를 입력해주세요',
+    limit: '해시태그는 5개까지 등록할 수 있습니다.',
+    validDuplmsg: '중복된 해시태그입니다.',
+  },
+  thumbnail: {
+    label: '썸네일',
+    desc: '플레이리스트 메인 썸네일을 선택해주세요.',
+  },
+  preview: {
+    large: '미리보기 (large)',
+    small: '미리보기 (small)',
+  },
+  createButton: { label: '플레이리스트 생성하기', loading: '생성 중...' },
+  toast: { success: '플레이리스트 생성이 완료되었습니다.' },
+};

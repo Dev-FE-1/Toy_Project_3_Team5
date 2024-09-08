6e63e8b6f10408fce1be9bc7862f99b65569b148
hjkwon <sirru97@naver.com>
2024-09-08 23:45:01 +0900

    fix: 댓글 프로필이미지 고정 #138

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -73,8 +73,10 @@ const commentStyles = css`
   align-items: center;
 
   img {
-    width: 36px;
-    height: 36px;
+    min-width: 36px;
+    max-width: 36px;
+    min-height: 36px;
+    max-height: 36px;
     border-radius: 50%;
     object-fit: cover;
     :hover {
